6156fccdf12ec2b75a40833df20f1bd3f135efed
rafaellucio <rafaelantoniolucio@gmail.com>
2014-11-11 12:49:47 -0200

    finished first and second chapter handout of Caelum

---
+++ LearningJava/src/Example1ErrorCompile/ErrorCompile.java
@@ -0,0 +1,10 @@
+package Example1ErrorCompile;
+
+public class ErrorCompile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Falta ponto e vérgula");
+	}
+
+}
